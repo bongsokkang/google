a659aa45340afbcad06fbc862b294d2e8e9184ec
Bongsok Kang <bongsokkang@gmail.com>
2019-08-08 18:42:58 -0700

    hello

--- src/components/Follow/Follow.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./Follow.css";
-
-const Follow = (props) =>(
-  <button onClick= {props.follow.bind(this, props.id)}className= "btn btn-info followbtn ">{props.following ? 'Unfollow': 'Follow' }</button>
-);
-export default Follow;
